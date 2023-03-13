ea30967f14d27201fbaa1aee9beb9716322fbd05
Jamila <jamnjeri@gmail.com>
2023-03-13 14:50:50 +0300

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,28 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = Student.all
+    students = if params[:name] 
+                  Student.find_by(first_name: params[:name])
+               else
+                  Student.all
+    end   
     render json: students
+  end
+
+  def grades
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+
+  def highest_grade
+      student = Student.order(grade: :desc).first
+      render json: student
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
